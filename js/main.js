71310773e2557f5dd4a70685e5da89912accc9d3
Darshan Nikam <darshannikam8055@gmail.com>
2022-08-29 21:05:30 +0530

    new changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
 //this is for login popup
 $(function () {
-
     $('a.btn-login').on('click',function () {
         $('.login-sec').toggleClass('active-popup');
     });
 });
 
-//this is for login main banner slide
+//this is for header
+$(function() {
+    $(window).on("scroll ", function() {
+      if ($(window).scrollTop() > 1) {
+          $(".header").addClass("header-fixed");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+          $(".header").removeClass("header-fixed");
+      }
+    });
+    });
 
+//this is for mobile
+
+$(document).ready(function(){
+    if ($(window).width() <= 991) {  
+        $(".hero-banner").addClass("mobile-banner");
+        }  
+    });
+
+    $(function() {
+        $(window).on("scroll ", function() {
+          if ($(window).scrollTop() > 1) {
+              $(".mobile-banner").addClass("mobile-banner-new");
+          } else {
+              //remove the background property so it comes transparent again (defined in your css)
+              $(".mobile-banner ").removeClass("mobile-banner-new");
+          }
+        });
+        });
 
